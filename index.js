abdb44e69e8ad9691faccf145d2c04425995e94e
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2022-08-15 15:03:28 +0000

    /add page

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ app.get("/", (req, res) => {
     res.send("<h1>Hey... You're not meant to be here!")
 });
 
+app.get(["/add", "/add:name:score"], (req, res) => {
+    if (name && score) {
+        res.send(name + ' and ' + score);
+    } else {
+        res.send("<h1>Try some arguments...")
+    }
+});
+
 app.listen(port, () => console.log(`Ominous Ocean Leaderboard is running on ${port}`));
