84599edb40b511541360c2b263452230110a34fc
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2022-08-17 12:46:06 +0000

    Created Password generator and sends back to user. Also added some logging

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const fs = require('fs');
 const bcrypt = require('bcrypt');
 
 app.get("/", (req, res) => {
+    console.log("Request for home page");
     res.send("Error Code 1: Trying to add to leaderboard without providing player name and/or score<br />Error Code 2: Tried to provide a score that wasn't a number<br />Error Code 3: Failed to read file on server side");
 });
 
 app.get(["/add", "/add/:player/:score"], (req, res) => {
+    console.log("Adding to leaderboard");
     player = req.params["player"];
     score = req.params["score"];
     if(isNaN(score)) {
@@ -20,13 +22,16 @@ app.get(["/add", "/add/:player/:score"], (req, res) => {
             place += 1
             if(!fs.existsSync('/leaderboard/' + place + '.json')) {
                 password = bcrypt.genSaltSync(25);
-                pwHashed = bcrypt.hashSync(password, password);
+                pwHashed = bcrypt.hashSync(password, 5);
+                console.log('Password Created and Hashed: ' + pwHashed);
                 let fileToSend = {
                     player: player,
                     score: score,
                     privateDeleteKey: pwHashed
                 };
-                res.send(password + fileToSend);
+                console.log("Score submitted and saved. Player name: " + player + '. Score: ' + score + '. Place: ' + place);
+                res.send(password);
+                return 0;
             }
             let leaderboardPosJson = fs.readFileSync('/leaderboard/' + place + '.json');
 
