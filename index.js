6e4a86f1f7ea4bc88beb34edf258df99eea84260
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2022-08-17 11:35:59 +0000

    Made password generator for deletion of score

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const fs = require('fs');
+const bcrypt = require('bcrypt');
 
 app.get("/", (req, res) => {
-    res.send("Error Code 1: Trying to add to leaderboard without providing player name and/or score\nError Code 2: Tried to provide a score that wasn't a number\nError Code 3: Failed to read file on server side");
+    res.send("Error Code 1: Trying to add to leaderboard without providing player name and/or score<br />Error Code 2: Tried to provide a score that wasn't a number<br />Error Code 3: Failed to read file on server side");
 });
 
 app.get(["/add", "/add/:player/:score"], (req, res) => {
@@ -18,7 +19,14 @@ app.get(["/add", "/add/:player/:score"], (req, res) => {
         while (place <= 1000) {
             place += 1
             if(!fs.existsSync('/leaderboard/' + place + '.json')) {
-                res.send('3')
+                password = bcrypt.genSaltSync(25);
+                pwHashed = bcrypt.hashSync(password);
+                let fileToSend = {
+                    player: player,
+                    score: score,
+                    privateDeleteKey: pwHashed
+                };
+                res.send(password + fileToSend);
             }
             let leaderboardPosJson = fs.readFileSync('/leaderboard/' + place + '.json');
 
