353ad56132a4c8209f24639d9f5618fb0463be6f
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2022-08-17 10:55:12 +0000

    Check if score is an integer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const fs = require('fs');
 
 app.get("/", (req, res) => {
     res.send("<h1>Hey... You're not meant to be here!")
@@ -9,10 +10,18 @@ app.get("/", (req, res) => {
 app.get(["/add", "/add/:player/:score"], (req, res) => {
     player = req.params["player"];
     score = req.params["score"];
+    if(!isNaN(score)) {
+        res.send("2");
+    }
     if (player && score) {
+        place = 0;
+        while (place <= 1000) {
+            place += 1
+            
+        }
         res.send(player + ' and ' + score);
     } else {
-        res.send("<h1>Try some arguments...");
+        res.send("1");
     }
 });
 
